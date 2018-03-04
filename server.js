1b1573c5a68f116a7a9f451161f3c005cd2b6281
Mark Andrey <markandreydelacruz@ymail.com>
2018-03-04 17:02:45 +0800

    added a database

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-let messages = [];
+const dbUrl = 'mongodb://user:1234@ds155278.mlab.com:55278/app-chat-express-js';
+
+
+const Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req, res) => {
-    res.send(messages);
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
 });
 
 app.post('/messages', (req, res) => {
-    console.log(req.body);
-    messages.push(req.body)
-    res.sendStatus(200);
+    const message = new Message(req.body);
+    message.save((err) => {
+        if(err) {
+            sendStatus(500)
+        }  
+        messages.push(req.body)
+        io.emit('message', req.body)
+        res.sendStatus(200);
+    })
 });
 
+io.on('connection', (socket) => {
+    console.log('user connected');
+});
 
-const server = app.listen(3000, () => {
+mongoose.connect(dbUrl, (err) => {
+    console.log('mondo db connection:', err);
+});
+
+const server = http.listen(3000, () => {
     console.log('Server is listening on port', server.address().port)
 });
